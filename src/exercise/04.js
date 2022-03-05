6e90b4224992a8a6f50be21a30632a01969d5dbb
Rakib544 <md.rakib10122003@gmail.com>
2022-03-05 09:59:38 +0600

    learning ablut props collection and getter

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,30 +4,45 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+function callAll(...fns) {
+  return (...args) => {
+    fns.forEach(fn => {
+      fn && fn(...args)
+    })
+  }
+}
+
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
+  function getTogglerProps({onClick, ...props} = {}) {
+    return {
+      'aria-pressed': on,
+      onClick: callAll(onClick, toggle),
+      ...props,
+    }
+  }
+
   return {
     on,
     toggle,
-    togglerProps: {
-      'aria-pressed': on,
-      onClick: toggle,
-    },
+    getTogglerProps,
   }
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
